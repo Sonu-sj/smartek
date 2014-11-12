fe6755b3ff8b3c4de7eea1cde07567b789e01a82
Sonu-sj <sonusamjoseph@gmail.com>
2014-11-12 12:51:41 +0530

    player hack

--- public/javascripts/dircontroller.js
+++ public/javascripts/dircontroller.js
@@ -1,37 +1,56 @@
-angular.module('dirtest', ['myService', 'ngTable','ui.bootstrap'])
-    .controller('dirtest', function($scope, testFactory, ngTableParams, $filter,$rootScope) {
-     $scope.loaddata = function()
-     {
-           testFactory.getdata().success(function(data1) {
-            $scope.testdata = data1;
-            var data = data1;
+angular.module('dirtest', ['myService', 'ngTable', 'ui.bootstrap'])
+    .controller('dirtest', function($scope, testFactory, ngTableParams, $filter, $rootScope, $timeout,$document) {
+          
 
-            $scope.tableParams = new ngTableParams({
-                page: 1, // show first page
-                count: 2, // count per page
-                
-            }, {
-                total: data.length, // length of data
 
-                getData: function($defer, params) {
-                    var orderedData = params.sorting() ?
-                        $filter('orderBy')(data, params.orderBy()) :
-                        data;
-                    $defer.resolve(orderedData.slice((params.page() - 1) * params.count(), params.page() * params.count()));
+        $scope.loaddata = function() {
+            testFactory.getdata().success(function(data1) {
+                $scope.testdata = data1;
+                var data = data1;
 
-                }
-            });
-        })
-     }
+                $scope.tableParams = new ngTableParams({
+                    page: 1, // show first page
+                    count: 2, // count per page
+
+                }, {
+                    total: data.length, // length of data
+
+                    getData: function($defer, params) {
+                        var orderedData = params.sorting() ?
+                            $filter('orderBy')(data, params.orderBy()) :
+                            data;
+                        $defer.resolve(orderedData.slice((params.page() - 1) * params.count(), params.page() * params.count()));
+
+                    }
+                });
+            })
+        }
         $scope.loaddata();
-        $rootScope.$on("myEvent", function (event, args) {
-           
-  $scope['tableParams'] = null;
-  $scope.loaddata();
-});
+     
+        $scope.toTestInput = function() {
+            console.log($scope.testInput);
+
+        }
+
+        $scope.play = function(sourceurl) {
+
+            $scope.song = {}
+            $scope.song.source = '/media.mp3';
+            $timeout(function() {
+                document.getElementById("myAudio").load();
+                document.getElementById("myAudio").play();
+            }, 0)
+
+        }
+        $rootScope.$on("myEvent", function(event, args) {
+
+
+            $scope['tableParams'] = null;
+            $scope.loaddata();
+        });
 
     })
-    .controller('modal', function($scope,$modal,$log) {
+    .controller('modal', function($scope, $modal, $log) {
 
         $scope.items = ['item1', 'item2', 'item3'];
 
@@ -54,24 +73,25 @@ angular.module('dirtest', ['myService', 'ngTable','ui.bootstrap'])
                 $log.info('Modal dismissed at: ' + new Date());
             });
         };
-    }
-    )
+    })
 
-.controller('ModalInstanceCtrl', function ($scope, $modalInstance, items,sendFactory,$rootScope) {
+.controller('ModalInstanceCtrl', function($scope, $modalInstance, items, sendFactory, $rootScope) {
 
-  $scope.items = items;
-  $scope.selected = {
-    item: $scope.items[0]
-  };
+    $scope.items = items;
+    $scope.selected = {
+        item: $scope.items[0]
+    };
 
-  $scope.ok = function () {
-    $modalInstance.close($scope.selected.item);
-   
-    sendFactory.postdata($scope.trackname).success(function(data){$rootScope.$broadcast("myEvent")});
+    $scope.ok = function() {
+        $modalInstance.close($scope.selected.item);
 
-  };
+        sendFactory.postdata($scope.trackname).success(function(data) {
+            $rootScope.$broadcast("myEvent")
+        });
 
-  $scope.cancel = function () {
-    $modalInstance.dismiss('cancel');
-  };
+    };
+
+    $scope.cancel = function() {
+        $modalInstance.dismiss('cancel');
+    };
 });
